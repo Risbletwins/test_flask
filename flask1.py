ac88d8c12b73fbdacb94c3fd46bae292c41bfdac
Nameless <nuslans958@gmail.com>
2025-06-20 10:34:16 +0600

    testing reqs 2

--- flask1.py
+++ flask1.py
@@ -14,14 +14,14 @@ def home():
 def get_data():
     return jsonify(current_data)
 
-# You can update the message using this endpoint
-@app.route("/set-data", methods=["POST"])
-def set_data():
-    data = request.json
-    if "message" in data:
-        current_data["message"] = data["message"]
-        return jsonify({"status": "success", "new_message": current_data["message"]})
-    return jsonify({"status": "error", "reason": "No message found"}), 400
+# # You can update the message using this endpoint
+# @app.route("/set-data", methods=["POST"])
+# def set_data():
+#     data = request.json
+#     if "message" in data:
+#         current_data["message"] = data["message"]
+#         return jsonify({"status": "success", "new_message": current_data["message"]})
+#     return jsonify({"status": "error", "reason": "No message found"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)  # Make sure it's accessible on your network
